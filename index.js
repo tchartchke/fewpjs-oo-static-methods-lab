d8713d6c8ff3dc8263b9a75a6dd79970efe5d280
tchartchke <chartai.kl@gmail.com>
2021-05-04 14:05:33 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string){
+    let words = string.split(' ')
+    words = words.map(word => {
+      const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      return exceptions.includes(word) ? word : Formatter.capitalize(word)
+    })
+    return Formatter.capitalize(words.join(' '))
+  }
 }
